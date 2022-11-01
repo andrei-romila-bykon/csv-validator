eece2977d2c08a5e5fca5c39ce833351ecb96db7
Andrei <andrei.romila@bykon.com.mx>
2022-11-01 15:04:27 +0100

    Reimplemented to accept empty lines or even a line without some fields.

--- src/main/java/com/csvvalidator/validators/data/Line.java
+++ src/main/java/com/csvvalidator/validators/data/Line.java
@@ -2,15 +2,33 @@ package com.csvvalidator.validators.data;
 
 public record Line(String id, String company, String contact, String rfc, String email, String foreign) {
 
-    public static Line toLine(String value) {
-        String[] fields = value.split(",");
+    public static Line toLine(final String value) {
 
-        // We MUST have 6 fields or the entire line is invalid
-        if (fields.length != 6) {
-            throw new IllegalStateException("The file is corrupted.");
-        }
+        // Split the line into fields
+        final String[] fields = value.split(",");
 
         // Here we can create a valid line
-        return new Line(fields[0], fields[1], fields[2], fields[3], fields[4], fields[5]);
+        return new Line(get(fields, 0),
+                get(fields, 1),
+                get(fields, 2),
+                get(fields, 3),
+                get(fields, 4),
+                get(fields, 5));
+    }
+
+    /**
+     * Simple method to avoid {@link NullPointerException} if there
+     * is a line with less than 6 fields
+     *
+     * @param fields {@link String}[] The list of fields
+     * @param index  {@link Integer} The index to access
+     * @return The value at the provided index or null if there is no index
+     */
+    private static String get(final String[] fields, final int index) {
+        if (index < fields.length) {
+            return fields[index];
+        }
+
+        return null;
     }
 }
